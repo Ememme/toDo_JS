1e956ff37ddd68624ab3b77249fa5ecad1d54c03
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-01-30 10:27:54 +0100

    Change to toDo object with methods

--- todo.js
+++ todo.js
@@ -1,10 +1,18 @@
-const todos = ['item 1', 'item2', 'item3'];
-
-function displayTodos(){
-  console.log("My todos:", todos);
-}
-
-function addTodos(todo) {
-  todos.push(todo);
-  displayTodos();
-}
+const toDoList = {
+  todos: ['item 1', 'item2', 'item3'],
+  displayToDos: function() {
+    console.log('My todos:', this.todos);
+  },
+  addToDo: function(todo) {
+    this.todos.push(todo);
+    this.displayToDos();
+  },
+  changeToDo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayToDos();
+  },
+  deleteToDo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayToDos();
+  }
+};
